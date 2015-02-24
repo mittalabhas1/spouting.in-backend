2efeb2cca30c794353001063846cf994da191da5
Abhas Mittal <mittalabhas1@gmail.com>
2015-02-24 19:22:56 +0530

    Minor refactor

--- sort/index.php
+++ sort/index.php
@@ -3,29 +3,13 @@ require_once('../partials/header.php');
 
 $categories = mysql_query("SELECT * FROM category ORDER BY `ord_no` ASC");
 
-if (isset($_POST['pages'])) {
-  $index = 0;
-  parse_str($_POST['pages'], $pageOrder);
-  var_dump($pageOrder);
-  exit(0);
-  foreach ($pageOrder['pages'] as $key => $value) {
-    mysql_query("UPDATE category SET `ord_no` = '$key' WHERE `id` = '$value'") or die(mysql_error());
-  }
-}
-
 ?>
-<style>
-  #sortables { list-style-type: none; margin: 0; padding: 0; width: 60%; }
-  #sortables li { margin: 3px 3px 3px 3px; padding-left: 1.5em; padding-top: 0.4em; padding-bottom: 0.4em; font-size: 1.4em; height: 1.8em; }
-  #sortables li span { position: absolute; margin-left: -1.3em; }
-</style>
 <script type="text/javascript">
   $(document).ready(function(){
     $('#sortables').sortable();
   });
-  function saveOrder(){
-    var data = $('#sortables').sortable('serialize');
-    console.log(data);
+  function saveOrder(sortThis){
+    var data = $('#sortables.'+sortThis).sortable('serialize');
     $.ajax({
       url: 'update.php',
       type: 'POST',
@@ -41,7 +25,7 @@ if (isset($_POST['pages'])) {
     });
   }
 </script>
-<ul id="sortables" class="sort-products user-select-none">
+<ul id="sortables" class="sort-products user-select-none categories">
   <?php 
   while ($category = mysql_fetch_assoc($categories)) {
   ?>
@@ -50,7 +34,7 @@ if (isset($_POST['pages'])) {
   }
   ?>
 </ul>
-<button class="btn btn-success m-l-xxs" onclick="saveOrder()">Save Order</button>
+<button class="btn btn-success m-l-xxs" onclick="saveOrder('categories')">Save Order</button>
 <div class="alert inline hidden" id="alert" style="padding: 10px;"></div>
 
 <?php
